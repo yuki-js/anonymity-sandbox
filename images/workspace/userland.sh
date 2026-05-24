d3fbca73d8d10c4a8d505be4838f09ace1fd0d09
yuki-js <me@aoki.app>
2026-05-24 18:58:45 +0900

    dotimageで作り直し

--- images/workspace/userland.sh
+++ images/workspace/userland.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
 
+unset DISPLAY
+export XDG_RUNTIME_DIR="${XDG_RUNTIME_DIR:-/runtime}"
+export GDK_BACKEND=wayland
+export MOZ_ENABLE_WAYLAND=1
+export LIBGL_ALWAYS_SOFTWARE=1
+export MESA_LOADER_DRIVER_OVERRIDE=llvmpipe
+export WINIT_UNIX_BACKEND=wayland
 
 # Launch Firefox with a profile selector in background
-/usr/bin/firefox -P &
+firefox -P &
 
 # launch alacritty terminal in background
 alacritty &
